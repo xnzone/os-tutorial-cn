56a13361912e76df907f2802135a981b78cfbca5
xnzone <xnzone@qq.com>
2021-03-15 11:38:20 +0800

    auto push

---
+++ 20-interrupts-timer/kernel/util.c
@@ -0,0 +1,42 @@
+#include "util.h"
+
+void memory_copy(char* source, char* dest, int nbytes) {
+    int i;
+    for(i = 0; i < nbytes; i++) {
+        *(dest + i) = *(source +i );
+    }
+}
+
+void memory_set(u8* dest, u8 val, u32 len) {
+    u8* temp = *dest;
+    for(; len != 0; len--) *temp++ = val;
+}
+
+/* K&R 实现 */
+void int_to_ascii(int n, char str[]) {
+    int i, sign;
+    if((sign = n) < 0 ) n = -n;
+    i = 0;
+    do {
+        str[i++] = n % 10 + '0';
+    } while((n /= 10) > 0);
+
+    if (sign < 0) str[i++] = '-';
+    str[i] = '\0';
+    reverse(str);
+}
+
+void reverse(char s[]) {
+    int c, i, j;
+    for(i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+void strlen(char s[]) {
+    int i = 0;
+    while(s[i] != '\0') ++i;
+    return i;
+}
