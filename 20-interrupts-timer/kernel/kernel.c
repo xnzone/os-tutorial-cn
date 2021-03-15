a009434c0b45d33e272c064dd5be3c3acbec7832
xnzone <xnzone@qq.com>
2021-03-15 14:34:36 +0800

    auto push

--- 20-interrupts-timer/kernel/kernel.c
+++ 20-interrupts-timer/kernel/kernel.c
@@ -2,9 +2,12 @@
 #include "util.h"
 #include "../cpu/isr.h"
 #include "../cpu/idt.h"
+#include "../drivers/keyboard.h"
 
 void main() {
     isr_install();
     asm volatile("sti");
     init_timer(50);
+    /* 键盘IRQs更简单 */
+    init_keyboard();
 }
