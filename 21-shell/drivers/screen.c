11dc8c10af9035b98805b74109b75bb96aaa1a16
xnzone <xnzone@qq.com>
2021-03-15 21:13:54 +0800

    auto push

--- 21-shell/drivers/screen.c
+++ 21-shell/drivers/screen.c
@@ -91,12 +91,12 @@ int print_char(char c, int col, int row, char attr) {
     if (offset >= MAX_ROWS * MAX_COLS * 2) {
         int i;
         for(i = 1; i < MAX_ROWS; i++) {
-            memory_copy(get_offset(0, i) + VIDEO_ADDRESS,
-            get_offset(0, i-1) + VIDEO_ADDRESS,
+            memory_copy((u8*)get_offset(0, i) + VIDEO_ADDRESS,
+            (u8*)get_offset(0, i-1) + VIDEO_ADDRESS,
             MAX_COLS * 2);
 
             /* 最后一行空着 */
-            char* last_line = get_offset(0, MAX_ROWS - 1) + VIDEO_ADDRESS;
+            char* last_line = (char*)get_offset(0, MAX_ROWS - 1) + VIDEO_ADDRESS;
             for(i = 0; i < MAX_COLS * 2; i++) last_line[i] = 0;
 
             offset -= 2 * MAX_COLS;
@@ -131,7 +131,7 @@ void set_cursor_offset(int offset) {
 void clear_screen() {
     int screen_size = MAX_ROWS * MAX_COLS;
     int i;
-    char* screen = VIDEO_ADDRESS;
+    u8* screen = (u8*)VIDEO_ADDRESS;
 
     for(i = 0; i < screen_size; i++) {
         screen[i*2] = ' ';
